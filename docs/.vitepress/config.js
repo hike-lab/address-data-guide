d0df0ea170defed8ede7820170d6cb91adf3f1bc
Jieun Ahn <68333747+An-JIeun@users.noreply.github.com>
2024-02-15 17:17:31 +0900

    vitepress setup

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,112 @@
+// https://vitepress.vuejs.org/config/#markdown-options
+
+export default {
+  lang: "ko-KR",
+  title: "Address Data Guide",
+  description: "Address Data Guide for HIKE Lab. at CAU",
+  base: "/docs/",
+
+  lastUpdated: true,
+  ignoreDeadLinks: true,
+
+  head: [['link', { rel: 'icon', href: '../static/icon/hikeLogo.svg' }]],
+
+  // markdown-it-footnote
+  markdown: {
+    lineNumbers: true,
+    math: true,
+    config: (md) => {
+      md.use(require("markdown-it-katex"));
+      md.use(require("markdown-it-footnote"));
+    },
+  },
+  build: { outDir: "/theme/" },
+  themeConfig: {
+    nav: nav(),
+    logo: "/static/icon/hikeLogo.svg",
+    
+    sidebar: {
+      "/": sidebarMain(),
+    },
+
+    editLink: {
+      pattern: "https://github.com/hike-lab/nlp-docs",
+      text: "Edit this page on GitHub",
+    },
+
+    socialLinks: [{ icon: "github", link: "https://github.com/hike-lab" }],
+
+    footer: {
+      // message: 'Docs are released under the MIT License.',
+      copyright: "Copyright © 2024 HIKE Lab.",
+    },
+
+    search: {
+      provider: "local",
+    },
+
+    // algolia: {
+    //   appId: '8J64VVRP8K',
+    //   apiKey: 'a18e2f4cc5665f6602c5631fd868adfd',
+    //   indexName: 'vitepress'
+    // }
+  },
+};
+
+function nav() {
+  return [
+    // { text: 'JustBoil.me', link: '/free/', activeMatch: '/free/' }
+    { text: "HIKE Lab.", link: "http://hike.cau.ac.kr" },
+  ];
+}
+
+function sidebarMain() {
+  return [
+
+    {
+      text: "1. 주소의 개념",
+      collapsed: true,
+      items: [
+        { text: "1. 주소란 무엇인가?", link: "/contents/chapter-1/chapter-1-1.html" },
+      ],
+    },
+
+    {
+      text: "2. 분석 환경 구축",
+      collapsed: true,
+      items: [
+        {
+          text: "0. 학습 자료 안내", link: "/contents/chapter-2/chapter-2-1.html",
+        },
+      ],
+    },
+    {
+      text: "3. 주소 데이터 EDA",
+      collapsed: true,
+      items: [
+        { text: "1. 주소란 무엇인가?", link: "/contents/chapter-3/chapter-3-1.html" },
+      ],
+    },
+    {
+      text: "4. 주소 데이터 API 활용한 검색 기능 구현",
+      collapsed: true,
+      items: [
+        { text: "1. 주소란 무엇인가?", link: "/contents/chapter-4/chapter-4-1.html" },
+      ]
+    },
+    {
+      text: "5. sql을 활용한 batch 데이터 쿼리",
+      collapsed: true,
+      items: [
+        { text: "1. 주소란 무엇인가?", link: "/contents/chapter-5/chapter-5-1.html" },
+      ]
+    },
+    {
+      text: "6. 도로명 주소의 구성과 품질",
+      collapsed: true,
+      items: [
+        { text: "1. 주소란 무엇인가?", link: "/contents/chapter-6/chapter-6-1.html" },
+      ]
+    },
+  ];
+}
