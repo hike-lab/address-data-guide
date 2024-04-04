ad1b1cc17f9d7d1be201b474873b4495f650c45f
hike-lab <hike.cau@gmail.com>
2024-04-04 11:46:56 +0900

    google analytics added

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -22,7 +22,14 @@ export default {
   lastUpdated: true,
   ignoreDeadLinks: true,
 
-  head: [["link", { rel: "icon", href: "../static/icon/hikeLogo.svg" }]],
+  head: [
+    ["link", { rel: "icon", href: "../static/icon/hikeLogo.svg" }],
+    ['script', {
+      async: true,
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-X21NCJHECK',
+    }],
+    ['script', {}, "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-X21NCJHECK');",]
+  ],
 
   // markdown-it-footnote
   markdown: {
